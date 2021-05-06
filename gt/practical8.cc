8e0e4ca640a4d2bb04563b547763f9c822e6d708
Tirth Patel <tirthasheshpatel@gmail.com>
2021-05-06 13:55:01 +0530

    Add all GT practicals

--- gt/practical8.cc
+++ gt/practical8.cc
@@ -4,5 +4,28 @@ using namespace std;
 
 int main()
 {
+    int v, e;
+    cout << "Enter no. of vertices in a graph: ";
+    cin >> v;
+    cout << "Enter no. of edges in a graph: ";
+    cin >> e;
+
+    if ( v%2 == 0 && e >= v*v/4 ) {
+        // graph is complete bi-partite
+        if ( e <= 2*v-4 ) {
+            cout << "Graph is planar" << endl;
+        }
+        else {
+            cout << "Graph is not planar" << endl;
+        }
+    }
+    if ( e <= 3*v-6 ) {
+        cout << "Graph is planar" << endl;
+    }
+    else {
+        cout << "Graph is not planar" << endl;
+    }
+
     return 0;
 }
+
